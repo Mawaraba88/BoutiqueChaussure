a934581edb0bf36119a85c25e97ab7ed2ec81f99
Mawaraba <mawaraba88@yahoo.fr>
2021-04-01 22:56:13 +0200

    Securisation du backoffice de l'admin

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -2,19 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\ResetPassword;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ResetPasswordController extends AbstractController
 {
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/mot-de-passe-oublie", name="reset_password")
      */
-    public function index(): Response
+    public function index(Request $request)
+
     {
-        return $this->render('reset_password/index.html.twig', [
-            'controller_name' => 'ResetPasswordController',
-        ]);
+        if($this->getUser()){
+            return $this->redirectToRoute('home');
+        }
+        if($request->get('email')){
+            $user = $this->entityManager->getRepository(User::class)
+                ->findOneByEmail($request->get('email'));
+            if($user){
+                //Etape 1 : Enregistrer en base la demande de reset_password avec user, token, createdAd.
+                $reset_password = new ResetPassword();
+                $reset_password->setUser($user);
+                $reset_password->setToken(uniqid());
+                $reset_password->setCreatedAt(new \DateTime());
+
+                $this->entityManager->persist($reset_password);
+                $this->entityManager->flush();
+
+                //2: Envoyer un email à l'user avec un lien lui permettant de mettre à jour son mot de passe
+            }
+        }
+        return $this->render('reset_password/index.html.twig');
+    }
+
+    /**
+     * @Route("/modifier-mon-mot-de-passe/{token}", name="update_password")
+     */
+    public function update($token){
+
     }
 }
