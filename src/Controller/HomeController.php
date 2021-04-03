6286b5613668633dcafea9971bc713f8abf44b5c
kaba <fkaba@live.fr>
2021-04-03 20:20:56 +0200

    Envoi de mail

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,9 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-
+        //test envoi de mail
+        //$mail = new Mail();
+        //$mail->send('anadyeven@gmail.com', 'fatou', 'Mom premier mail', "Bonjour Fatou, j'espère que tu vas bien");
         return $this->render('home/index.html.twig');
     }
 }
